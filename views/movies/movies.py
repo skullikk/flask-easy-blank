7c075bc22470e85d0b638a9fc763444d2ebb25a7
skullikk <skullikk@mail.ru>
2022-10-13 13:58:59 +0300

    Add output json

--- views/movies/movies.py
+++ views/movies/movies.py
@@ -14,27 +14,19 @@ class MoviesView(Resource):
         director_id = request.args.get('director_id')
         genre_id = request.args.get('genre_id')
         year = request.args.get('year')
-        # Если есть ключ director_id - получаем его и выводим список фильмов. Если список пустой - выводим "Empty"
+        # Если есть ключ director_id - получаем его и выводим список фильмов. Если список пустой - выводим пустой список
         if director_id:
             movies = Movie.query.filter(Movie.director_id == director_id).all()
-            if movies:
-                return movies_schema.dump(movies), 200
-            else:
-                return 'Empty', 200
-        # Если есть ключ genre_id - получаем его и выводим список фильмов. Если список пустой - выводим "Empty"
+            return movies_schema.dump(movies), 200
+        # Если есть ключ genre_id - получаем его и выводим список фильмов. Если список пустой - выводим пустой список
         elif genre_id:
             movies = Movie.query.filter(Movie.genre_id == genre_id).all()
-            if movies:
-                return movies_schema.dump(movies), 200
-            else:
-                return 'Empty', 200
-        # Если есть ключ year - получаем его и выводим список фильмов. Если список пустой - выводим "Empty"
+            return movies_schema.dump(movies), 200
+        # Если есть ключ year - получаем его и выводим список фильмов. Если список пустой - выводим пустой список
         elif year:
             movies = Movie.query.filter(Movie.year == year).all()
-            if movies:
-                return movies_schema.dump(movies), 200
-            else:
-                return 'Empty', 200
+            return movies_schema.dump(movies), 200
+
 
         else:
             all_movies = Movie.query.all()
@@ -42,14 +34,10 @@ class MoviesView(Resource):
 
     def post(self):
         req_json = request.json
-        movies = Movie.query.filter(Movie.id == req_json.get("id")).all()
-        if movies:
-            return "Conflict id", 409
-        else:
-            new_movie = Movie(**req_json)
-            db.session.add(new_movie)
-            db.session.commit()
-        return "Create", 201, {"location": f"/movies/{new_movie.id}"}
+        new_movie = Movie(**req_json)
+        db.session.add(new_movie)
+        db.session.commit()
+        return movie_schema.dump(new_movie), 201, {"location": f"/movies/{new_movie.id}"}
 
 @movie_ns.route('/<int:mid>')
 class MovieView(Resource):
@@ -57,28 +45,25 @@ class MovieView(Resource):
         movie = Movie.query.get(mid)
         return movie_schema.dump(movie), 200
 
+
     def put(self, mid):
         movie = Movie.query.get(mid)
-        if movie:
-            req_json = request.json
-            movie.title = req_json.get("title")
-            movie.description = req_json.get("description")
-            movie.trailer = req_json.get("trailer")
-            movie.year = req_json.get("year")
-            movie.rating = req_json.get("rating")
-            movie.genre_id = req_json.get("genre_id")
-            movie.director_id = req_json.get("director_id")
-            db.session.add(movie)
-            db.session.commit()
-            return "Done", 200
-        else:
-            return "Empty", 404
+        req_json = request.json
+        movie.title = req_json.get("title")
+        movie.description = req_json.get("description")
+        movie.trailer = req_json.get("trailer")
+        movie.year = req_json.get("year")
+        movie.rating = req_json.get("rating")
+        movie.genre_id = req_json.get("genre_id")
+        movie.director_id = req_json.get("director_id")
+        db.session.add(movie)
+        db.session.commit()
+        return movie_schema.dump(movie), 200
+
 
     def delete(self, mid):
         movie = Movie.query.get(mid)
-        if movie:
-            db.session.delete(movie)
-            db.session.commit()
-            return "Done", 200
-        else:
-            return "Empty", 404
+        db.session.delete(movie)
+        db.session.commit()
+        return movie_schema.dump(Movie.query.get(mid)), 204
+
